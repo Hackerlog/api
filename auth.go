de441db675960384ed87ee0ba8602d918f7d22d1
dericgw <dcain@guidewire.com>
2018-07-21 22:37:49 -0500

    Added password reset post

--- auth.go
+++ auth.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/pborman/uuid"
 	log "github.com/sirupsen/logrus"
+	mailgun "gopkg.in/mailgun/mailgun-go.v1"
 )
 
 // Auth These are the users' JWT's
@@ -23,6 +27,7 @@ type Auth struct {
 // AuthRoutes Register the routes
 func AuthRoutes(r *gin.RouterGroup) {
 	r.POST("/login", checkAuth)
+	r.POST("/password-reset", passwordReset)
 }
 
 // @Summary Authenticates a user
@@ -90,4 +95,56 @@ func addJwtToDb(j *JWT, userID uint) {
 	}
 
 	db.Create(&userToken)
+}
+
+// ResetRequest This is the reset password request
+type ResetRequest struct {
+	Email string `json:"email"`
+}
+
+func passwordReset(c *gin.Context) {
+	var req ResetRequest
+	var user User
+	var res GenericResponse
+	res.Error = ""
+	res.Success = true
+
+	c.ShouldBindJSON(&req)
+
+	db := GetDb()
+
+	if err := db.Where("email = ?", req.Email).First(&user).Error; err != nil {
+		c.JSON(http.StatusOK, res)
+	} else {
+		token := uuid.New()
+		user.PasswordResetToken = token
+		db.Save(&user)
+		if _, err = sendResetEmail(user.Email, token); err != nil {
+			c.AbortWithError(http.StatusBadRequest, err)
+		}
+		c.JSON(http.StatusOK, res)
+	}
+}
+
+func sendResetEmail(email string, resetKey string) (string, error) {
+	template, err := ioutil.ReadFile("emails/reset-password.html")
+	if err != nil {
+		log.Error("Could not read email template file", err)
+	}
+	sTemplate := string(template)
+
+	mg := mailgun.NewMailgun("hello.hackerlog.io", os.Getenv("MAILGUN_API_KEY"), "")
+	m := mg.NewMessage(
+		"Hackerlog Robot <robot@hello.hackerlog.io>",
+		"Password Reset",
+		"Password Reset",
+		email,
+	)
+	html := strings.Replace(sTemplate, "%URL%", os.Getenv("APP_URL")+"/v1/auth/reset-password/"+resetKey, 1)
+	m.SetHtml(html)
+	_, id, err := mg.Send(m)
+	if err != nil {
+		log.Error("Password Reset email did not send", err)
+	}
+	return id, err
 }
